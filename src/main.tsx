039b631b458d57f600a9d037de59ec66fcbe1c04
Anton Henriksson <henriksson.anton@gmail.com>
2026-09-15 22:23:12 +0200

    added form to /login

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import HomePage from './pages/HomePage.tsx'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
